74c495bfeb49968178cefe48627f82e748eb37f0
almarelo <31460193+almarelo@users.noreply.github.com>
2021-09-17 06:15:01 +0000

    final

--- listas.py
+++ listas.py
@@ -5,33 +5,73 @@ lifestore_searches = [id_search, id product]
 lifestore_sales = [id_sale, id_product, score (from 1 to 5), date, refund (1 for true or 0 to false)]
 lifestore_products = [id_product, name, price, category, stock]
 
-Some new useful lists:
+Se creó la nueva lista productos_ventas, la cual por cada producto, registra cantidad de ventas, cantidad de devoluciones, puntuación promedio, si tiene o no puntuación y el número de búsquedas.
 
-productos_ventas = [id_product, ventas, refunds, ventas-refunds]
+productos_ventas = [producto[], ventas, refunds, ventas- refunds, score_promedio, puntuado, searches]
 
-mas_vendidos = [id_product, name, ventas, refunds ]
 """
 
 
-
-from lifestore_file import lifestore_products, lifestore_sales, lifestore_searches
+from lifestore_file import lifestore_products, lifestore_sales, lifestore_searches 
+'se importan las listas originales con los datos de la empresa'
 
 from operator import itemgetter, attrgetter
+'se importan estos operadores para poder ordenas listas respecto a un índice particular'
 
 
 productos_ventas=[]
+'variable para almacenar la lista de productos'
 
 for producto in lifestore_products:
+  'se va a crear un elemento por cada producto existente en la tienda y cada uno tendrá asignados los siguientes parámetros'
   sales=0
   refunds =0
+  suma_scores = 0
+  searches = 0
+  
   for venta in lifestore_sales:
+    'ciclo para contar el número de ventas y el número de devoluciones con los datos de la lista lifestore_sales'
     if producto[0]== venta[1]:
       sales += 1
       refunds += venta[4]
-  productos_ventas.append([producto, sales, refunds, sales - refunds])
+      suma_scores += venta[2]
+  if (sales == 0): 
+    'en caso de no tener ventas, no tendrá puntuación'
+    puntuado = 0
+    score_promedio = 'N/A'  
+  else:  
+    'en caso de tener ventas, se almacena la puntuación promedio de todas las ventas'
+    puntuado = 1
+    score_promedio = suma_scores/sales
+  for search in lifestore_searches:
+    if producto[0] == search[1]:
+      searches += 1
+  
+  'se almacena el elemento con todos sus atributos'
+  productos_ventas.append([producto, sales, refunds, sales - refunds, score_promedio, puntuado, searches])
 
 
-
+'se almacenan en una nueva lista los 10 más vendidos en orden'
 mas_vendidos = sorted(productos_ventas, key=itemgetter(3), reverse=True)[0:10]
 
+'se almacenan en una nueva lista los 10 menos vendidos en orden'
 menos_vendidos = sorted(productos_ventas, key=itemgetter(3))[0:10]
+
+productos_puntuados = []
+
+'para las listas referentes a puntuación, solo se considerarán los productos que sí están puntuados'
+for producto in productos_ventas:
+  if producto[5]:
+    productos_puntuados.append(producto) 
+
+'se almacenan en una nueva lista los 10 mejores puntuados'
+mejor_puntuados = sorted(productos_puntuados, key=itemgetter(4), reverse= True)[0:10]
+
+'se almacenan en una nueva lista los 10 peores puntuados'
+peor_puntuados = sorted(productos_puntuados, key=itemgetter(4))[0:10]
+
+'se almacenan en una nueva lista los 10 más buscados'
+mas_buscados = sorted(productos_ventas, key=itemgetter(6), reverse=True)[0:10]
+
+'se almacenan en una nueva lista los 10 menos buscados'
+menos_buscados = sorted(productos_ventas, key=itemgetter(6))[0:10]
--- main.py
+++ main.py
@@ -1,21 +1,18 @@
 from lifestore_file import lifestore_products, lifestore_sales, lifestore_searches
+'se importan las listas originales de los datos de la empresa'
 
-from listas import productos_ventas, mas_vendidos, menos_vendidos
+from listas import productos_ventas, productos_puntuados, mas_vendidos, menos_vendidos, mejor_puntuados, peor_puntuados, mas_buscados, menos_buscados
+'se importan las listas auxiliares que fueron creadas para fines del análisis'
 
 
-
-print(menos_vendidos)
-
-print ('Hello, World!')
-
-print ()
-
+'lista de parejas [usuario, contraseña] que tienen acceso al sistema'
 lista_permitidos = [['usuario', 'password'],['usuario2', '1234'],['usuario3', '8JDk&3t'],['usuario4', 'password4']]
 
+'variables auxiliares para determinar cuando puede ingresar el usuario y cuando hizo demasiados intentos'
 acceso = False
 intentos_fallidos = 0
 
-
+'este ciclo se repite mientras no se ingresen datos correctos pero no se pase de 3 intentos'
 while (acceso == False and intentos_fallidos < 3):
 
  usuario = input("Ingrese su nombre de usuario: ")
@@ -36,33 +33,94 @@ if acceso:
 if intentos_fallidos == 3:
   print ('Demasiados intentos fallidos. Acceso no permitido.')
 
-acceso = False
+'variables auxiliares para determinar cuándo se muestra el menú'
+salir = False
 intentos_fallidos = 0
 
-while(acceso == False and intentos_fallidos < 3):
+'este ciclo se repite mientras el usuario no decida salir y no tenga más de 3 intentos fallidos de ingreso'
+while(salir == False and intentos_fallidos < 3):
  print('MENÚ DE OPCIONES: ')
  print(' 1 Productos más vendidos.')
- print(' 2 Productos más buscados. ')
- print(' 3 Productos menos vendidos.')
- print(' 4 Productos menos buscados. ')
+ print(' 2 Productos menos vendidos.')
+ print(' 3 Productos más buscados.')
+ print(' 4 Productos menos buscados.')
+ print(' 5 Productos mejor puntuados.')
+ print(' 6 Productos peor puntuados.')
  print(' 0 Salir')
  
+
+ 'al elegir una opcion del menú se muestran los datos relevantes de la lista elegida y siempre se da la opción de salir o volver al menú'
  seleccion = input('Elija una opción del menú ingresando el número correspondiente: ')
- if (seleccion == '0' or seleccion == '1' or seleccion == '2'or seleccion == '3' or seleccion == '4'):
-   acceso = True 
-   
+ if seleccion == '0':
+   salir = True 
+ elif seleccion == '1':
+    print('Los productos más vendidos son:')
+    for i in range(10):
+     print(i+1) 
+     print(mas_vendidos[i][0][1])
+     print('Ventas: ', mas_vendidos[i][1])
+     print('Devoluciones: ', mas_vendidos[i][2])
+    salir = (input('Ingresa cualquier número para volver al menú o 0 para salir:  ')== '0') 
+    
+ elif seleccion == '2':
+    print('Los productos menos vendidos son:')
+    for i in range(10):
+     print(i+1) 
+     print(menos_vendidos[i][0][1])
+     print('Ventas: ', menos_vendidos[i][1])
+     print('Devoluciones: ', menos_vendidos[i][2] )
+
+    salir = (input('Ingresa cualquier número para volver al menú o 0 para salir:  ')== '0') 
+ elif seleccion == '3':
+    print('Los productos más buscados son:')
+    for i in range(10):  
+     print(i+1) 
+     print(mas_buscados[i][0][1])
+     print('Búsquedas: ', mas_buscados[i][6])
+     print('Ventas: ', mas_buscados[i][1])
+     print('Devoluciones: ', mas_buscados[i][2])
+    salir = (input('Ingresa cualquier número para volver al menú o 0 para salir:  ')== '0')  
+ elif seleccion == '4':
+    print('Los productos menos buscados son:')
+    for i in range(10):
+     print(i+1) 
+     print(menos_buscados[i][0][1])
+     print('Búsquedas: ', menos_buscados[i][6])
+     print('Ventas: ', menos_buscados[i][1])
+     print('Devoluciones: ', menos_buscados[i][2])
+    salir = (input('Ingresa cualquier número para volver al menú o 0 para salir:  ')== '0') 
+ elif seleccion == '5':
+    print('Los productos mejor puntuados son:')
+    for i in range(10):
+     print(i+1) 
+     print(mejor_puntuados[i][0][1])
+     print('Puntuación promedio: ', mejor_puntuados[i][4] )
+     print('Búsquedas: ', mejor_puntuados[i][6])
+     print('Ventas: ', mejor_puntuados[i][1])
+     print('Devoluciones: ', mejor_puntuados[i][2])
+    salir = (input('Ingresa cualquier número para volver al menú o 0 para salir:  ')== '0')  
+ elif seleccion == '6':
+    print('Los productos peor puntuados son:')
+    for i in range(10):
+     print(i+1) 
+     print(peor_puntuados[i][0][1])
+     print('Puntuación promedio: ', peor_puntuados[i][4] )
+     print('Búsquedas: ', peor_puntuados[i][6])
+     print('Ventas: ', peor_puntuados[i][1])
+     print('Devoluciones: ', peor_puntuados[i][2])
+    salir = (input('Ingresa cualquier número para volver al menú o 0 para salir:  ')== '0') 
  else:
    print ('Opcion inexistente.')
    intentos_fallidos += 1
    if intentos_fallidos < 3:
      print('Intente de nuevo.')
 
-if acceso:
-  print('Usted seleccionó la opción: ' + seleccion)
+
+
 
 if intentos_fallidos == 3:
   print ('Demasiados intentos fallidos. Acceso no permitido.')
   
-if (seleccion == '0'):
+if (salir):
   print ('La sesión ha terminado.' )
 
